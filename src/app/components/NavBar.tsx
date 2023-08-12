baaec7815a0cdc41e778db27146da69b298a9e83
devGustavoR - Trampo <devgustavor@gmail.com>
2023-08-12 11:40:46 -0300

    NavPronto, só falta a parte Mobile

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,32 +1,46 @@
 import Image from "next/image";
+import Link from "next/link";
 import logoLight from "../assets/logoLight.png";
 
 export function NavBar() {
   return (
     <>
-      <nav className="flex justify-between gap-1 p-3">
+      {/* Falta só fazer a parte Mobile do NavBar */}
+      <nav className="flex justify-between gap-1">
         <a href="https://github.com/devGustavoR" target="_blank">
           <Image src={logoLight} width={80} height={80} alt="devGustavoR" />
         </a>
 
         <div>
-          <ul className="flex justify-between gap-12">
+          <ul className="flex justify-center gap-12 items-center pt-5 pr-5">
             <li>
-              <a href="" className="font-mono">
+              <Link href="#" className="hover:text-brown-400">
                 Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#Sobre"
+                className="hover:text-brown-400"
+                scroll={true}
+              >
+                Sobre
+              </Link>
+            </li>
+            <li>
+              <a href="" className="hover:text-brown-400">
+                Habilidades
               </a>
             </li>
             <li>
-              <a href="">Sobre</a>
+              <a href="" className="hover:text-brown-400">
+                Qualificações
+              </a>
             </li>
             <li>
-              <a href="">Habilidades</a>
-            </li>
-            <li>
-              <a href="">Qualificações</a>
-            </li>
-            <li>
-              <a href="">Contatos</a>
+              <a href="" className="hover:text-brown-400">
+                Contatos
+              </a>
             </li>
           </ul>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { Contatos } from "./components/Contatos";
 import { NavBar } from "./components/NavBar";
+import { Sobre } from "./components/Sobre";
 
 export default function Home() {
   return (
     <>
       <NavBar />
+      <Contatos />
+      <Sobre />
     </>
   );
 }
