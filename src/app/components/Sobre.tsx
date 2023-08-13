2e47368e0e7638f26470c9aab1cbf3bf2fb97c76
devGustavoR <devgustavor@gmail.com>
2023-08-13 19:09:25 -0300

    Texto do sobre adicionado

--- src/app/components/Sobre.tsx
+++ src/app/components/Sobre.tsx
@@ -5,7 +5,20 @@ export function Sobre() {
       <div></div>
 
       {/* Sobre mim */}
-      <div></div>
+      <div className="flex flex-col justify-center items-center">
+        {/* Who am i HELLO */}
+        <div></div>
+        <div>
+          <p>
+            Sou um Desenvolvedor Front-end com experiência em criação de
+            interfaces atraentes e interativas para web. Minha paixão por
+            programação começou quando descobri a habilidade de dar vida a
+            ideias criativas por meio de códigos. Hoje, estou em constante busca
+            por aprimoramento técnico para poder desenvolver projetos completos
+            e complexos, atuando também como desenvolvedor full-stack.{" "}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
