1c8749ec60c9d7aac970da3f5e42c902da3859f6
devGustavoR - Trampo <devgustavor@gmail.com>
2023-08-17 16:24:08 -0300

    Remove prettier and About in progress

--- src/app/components/Sobre.tsx
+++ src/app/components/Sobre.tsx
@@ -1,12 +1,12 @@
-import Image from 'next/image'
-import myPicture from '../assets/myPicture.jpg'
+import Image from "next/image";
+import myPicture from "../assets/myPicture.jpg";
 
 export function Sobre() {
   return (
     <>
       <section id="Sobre" className="section">
         <div className="flex justify-center items-center flex-col">
-          <h1 className="text-xl font-sans absolute left-[47%] text-center ">
+          <h1 className="text-2xl font-sans absolute left-[47%] mt-6 text-center font-black">
             About me
           </h1>
           <h1 className="text-9xl left-2 font-sans relative text-gray-500 font-black opacity-10">
@@ -16,9 +16,9 @@ export function Sobre() {
         <div className="flex justify-center items-center">
           {/*   Lado esquerdo */}
           <div className="px-28 py-16">
-            <div className="w-[300px] h-[400px] bg-gray-500 ml-2 absolute rounded"></div>
+            <div className="w-[300px] h-[400px] bg-gray-500 ml-2 absolute rounded-xl"></div>
             <Image
-              className="ml-2 relative top-10 left-10 rounded"
+              className="ml-2 relative top-10 left-10 rounded-xl"
               src={myPicture}
               width={1200}
               height={1200}
@@ -27,19 +27,19 @@ export function Sobre() {
           </div>
 
           {/* Lado Direito */}
-          <div className="flex flex-col p-16">
+          <div className="flex flex-col p-14">
             {/*   */}
             <div className="mb-5 flex items-center">
               <h2 className="justify-items-end text-xl">My name is</h2>
               <div className="left-2">
-                <div className="w-16 h-3  bg-brown-300 absolute mt-[14px] rounded-md opacity-40"></div>
-                <h1 className="font-bold text-xl tracking-normal font-alt relative left-2">
+                <div className="w-20 h-2  bg-brown-300 absolute mt-[16px] ml-2 rounded-md opacity-20"></div>
+                <h1 className="font-bold text-xl tracking-normal font-alt relative left-2	">
                   Gustavo
                 </h1>
               </div>
             </div>
 
-            <div>
+            <div className="flex flex-col items-center">
               <p className="text-justify font-sans mb-4 tracking-wide	">
                 Sou um Desenvolvedor Front-end apaixonado pela arte de criar
                 interfaces web atraentes e interativas. Minha jornada na
@@ -48,7 +48,7 @@ export function Sobre() {
                 aprimoramento técnico me levou a evoluir para um Desenvolvedor
                 Full-stack. Hoje, minha missão é unir a estética visual à
                 funcionalidade sólida, criando projetos completos que
-                proporcionem experiências digitais verdadeiramente memoráveis.{' '}
+                proporcionem experiências digitais verdadeiramente memoráveis.{" "}
               </p>
               <p className="text-justify font-sans tracking-wide">
                 Comecei como Desenvolvedor Front-end, onde descobri minha paixão
@@ -58,12 +58,39 @@ export function Sobre() {
                 comprometido em harmonizar a criatividade do design com a
                 solidez do desenvolvimento, criando projetos abrangentes que
                 deixam uma marca duradoura por meio de experiências digitais
-                excepcionais.
+                excepcionais.{" "}
               </p>
+
+              <div className="flex justify-center items-center mt-4">
+                <div className="flex flex-col items-center justify-center p-2">
+                  <div className="border-gray-300 border-solid border rounded-full w-16 h-16 flex justify-center items-center mb-2 ">
+                    <h2 className="text-brown-400 text-lg ">3</h2>
+                  </div>
+                  <p className="text-center leading-relaxed w-64 mr-4">
+                    Projetos concluídos no curso de DS
+                  </p>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <div className="border-gray-300 border-solid border rounded-full w-16 h-16 flex justify-center items-center mb-2 ">
+                    <h2 className="text-brown-400 text-lg ">2</h2>
+                  </div>
+                  <p className="text-center leading-relaxed w-64 mr-4">
+                    Conclusão do curso de desenvolvimento de sistema
+                  </p>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <div className="border-gray-300 border-solid border rounded-full w-16 h-16 flex justify-center items-center mb-2 ">
+                    <h2 className="text-brown-400 text-lg ">4</h2>
+                  </div>
+                  <p className="text-center leading-relaxed w-64 mr-4">
+                    Projetos pessoais concluídos
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
