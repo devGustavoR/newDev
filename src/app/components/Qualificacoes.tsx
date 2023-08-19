c40a6bae5a3e453718b7de1051410c86b6f066bb
devGustavoR - Trampo <devgustavor@gmail.com>
2023-08-19 10:50:39 -0300

    Qualifications in progress

--- src/app/components/Qualificacoes.tsx
+++ src/app/components/Qualificacoes.tsx
@@ -1,7 +1,14 @@
 export function Qualificacoes() {
   return (
     <div id="Qualificacoes" className="section">
-      Qualificações
+      <div className="flex justify-center items-center flex-col">
+        <h1 className="text-2xl font-sans absolute left-[45%] mt-6 text-center font-black">
+          QUALIFICATIONS
+        </h1>
+        <h1 className="text-8xl left-2 font-sans relative text-gray-500 font-black opacity-10">
+          QUALIFICATIONS
+        </h1>
+      </div>
     </div>
-  );
+  )
 }
