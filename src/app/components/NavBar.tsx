9ff02239072f07a4e6e7bd191af0c8c56f257e85
devgustavor - Trampo <devgustavor@gmail.com - Trampo>
2024-02-02 11:48:56 -0300

    NavBar responsive

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -7,11 +7,15 @@ import { BsInstagram } from 'react-icons/bs'
 import logoLight from '../assets/logoLight.png'
 
 export function NavBar() {
-  const [open, setOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleNav = () => {
+    setMenuOpen(!menuOpen)
+  }
+
   return (
     <>
-      {/* Falta só fazer a parte Mobile do NavBar */}
-      <nav className="flex justify-between items-center fixed gap-[36rem] z-[100] bg-gray-900 font-alt shadow-md shadow-brown-50/5 xl:gap-[30rem] md:gap-[25rem]">
+      <nav className="flex justify-between items-center fixed z-[100] bg-gray-900 font-alt shadow-md shadow-brown-50/5  w-[100%]">
         <a
           href="https://github.com/devGustavoR"
           target="_blank"
@@ -21,7 +25,7 @@ export function NavBar() {
           <Image src={logoLight} width={80} height={80} alt="devGustavoR" />
         </a>
 
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center max-lg:hidden">
           <ul className="flex justify-center gap-9 items-center">
             <li>
               <Link
@@ -29,7 +33,7 @@ export function NavBar() {
                 className="font-display hover:text-brown-400 flex justify-center items-center"
                 scroll={true}
               >
-                ABOUT ME
+                Sobre
               </Link>
             </li>
             <li>
@@ -38,7 +42,7 @@ export function NavBar() {
                 className="font-display hover:text-brown-400"
                 scroll={true}
               >
-                SKILLS
+                Habilidades
               </Link>
             </li>
             <li>
@@ -47,7 +51,7 @@ export function NavBar() {
                 className="font-display hover:text-brown-400"
                 scroll={true}
               >
-                QUALIFICATIONS
+                Qualificações
               </Link>
             </li>
             <li>
@@ -56,13 +60,13 @@ export function NavBar() {
                 className="font-display hover:text-brown-400"
                 scroll={true}
               >
-                CONTACT
+                Contatos
               </Link>
             </li>
           </ul>
         </div>
 
-        <div className="flex justify-center items-center gap-4  pr-3 ">
+        <div className="flex justify-center items-center gap-4  pr-3 max-lg:hidden">
           <Link href="https://github.com/devGustavoR">
             <AiFillGithub className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:text-brown-400 duration-300 w-6 h-6 mb-3 " />
           </Link>
@@ -72,6 +76,77 @@ export function NavBar() {
           <Link href="https://www.instagram.com/g_ribeir_/">
             <BsInstagram className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:text-brown-400 duration-300 w-6 h-6 mb-3" />
           </Link>
+        </div>
+
+        {/* Menu */}
+        <div
+          className={
+            menuOpen
+              ? 'max-lg:hidden'
+              : 'flex-col mr-3 hidden max-lg:flex cursor-pointer'
+          }
+          onClick={handleNav}
+        >
+          <i className="bg-white w-7 h-1 rounded-sm"></i>
+          <i className="bg-white w-7 h-1 rounded-sm mt-1"></i>
+          <i className="bg-white w-7 h-1 rounded-sm mt-1"></i>
+        </div>
+
+        <div
+          className={
+            menuOpen
+              ? 'fixed left-0 top-0 w-[100%] hidden h-screen bg-gray-50 p-10 ease-in duration-500 max-lg:flex max-lg:flex-col'
+              : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
+          }
+          onClick={handleNav}
+        >
+          <div className="max-lg:flex max-lg:flex-col left-[100%]">
+            <i className="bg-gray-600 w-8 h-[2px] rounded rotate-45 max-lg:cursor-pointer"></i>
+            <i className="bg-gray-600 w-8 h-[2px] rounded -rotate-45 -translate-y-1 max-lg:cursor-pointer"></i>
+          </div>
+          <div className="flex justify-center items-center mt-[25%]">
+            <ul className="flex justify-center gap-9 items-center flex-col">
+              <li>
+                <Link
+                  href="#Sobre"
+                  className="font-display hover:text-brown-400 text-gray-600 text-xl"
+                  scroll={true}
+                >
+                  Sobre
+                </Link>
+              </li>
+
+              <li>
+                <Link
+                  href="#Habilidades"
+                  className="font-display hover:text-brown-400 text-gray-600 text-xl"
+                  scroll={true}
+                >
+                  Habilidades
+                </Link>
+              </li>
+
+              <li>
+                <Link
+                  href="#Qualificacoes"
+                  className="font-display hover:text-brown-400 text-gray-600 text-xl"
+                  scroll={true}
+                >
+                  Qualificações
+                </Link>
+              </li>
+
+              <li>
+                <Link
+                  href="#Contatos"
+                  className="font-display hover:text-brown-400 text-gray-600 text-xl"
+                  scroll={true}
+                >
+                  Contatos
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,15 @@
-import { Contatos } from './components/Contatos'
-import { Habilidades } from './components/Habilidades'
-import { Inicio } from './components/Inicio'
 import { NavBar } from './components/NavBar'
-import { Qualificacoes } from './components/Qualificacoes'
-import { Sobre } from './components/Sobre'
-import { Violino } from './components/Violino'
 
 export default function Home() {
   return (
     <>
       <NavBar />
-      <Inicio />
+      {/* <Inicio />
       <Sobre />
-      <Violino />
+      <Violino /> 
       <Habilidades />
       <Qualificacoes />
-      <Contatos />
+      <Contatos /> */}
       {/* {/* <ScrollToTop /> */}
     </>
   )
